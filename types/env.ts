9526f4e26f4f740843d89106f70080ca9742ef4f
SIDDHARTH RAMESH CHAND RAJA <45534008+siddharthrajaraja@users.noreply.github.com>
2022-04-25 10:40:56 +0530

    documentation: add docstrings for env.ts (#7)
    * documentation: add EXPRESS_PORT for env.ts
    
    * documentation: add docstrings for env.ts

--- types/env.ts
+++ types/env.ts
@@ -1,8 +1,23 @@
 interface ENV {
+  /**
+   * EXPRESS port where express server runs on machine
+   */
   EXPRESS_PORT: string;
+  /**
+   * POSTGRES username
+   */
   POSTGRES_USER: string;
+  /**
+   * POSTGRES password
+   */
   POSTGRES_PASSWORD: string;
+  /**
+   * POSGRES database name
+   */
   POSTGRES_DB: string;
+  /**
+   * JWT secret required by the JWT Auth service
+   */
   JWT_SECRET: string;
 }
 
