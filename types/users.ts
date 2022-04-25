81fb6da0f202a820393301e93c396bd984dd198a
siddharthrajaraja <siddharthraja9849@gmail.com>
2022-04-25 09:45:19 +0530

    documentation: add docstrings for User

--- types/users.ts
+++ types/users.ts
@@ -1,7 +1,19 @@
 interface User {
+  /**
+   * Regsitered user's username
+   */
   username: string;
+  /**
+   * Registered user's password
+   */
   password: string;
+  /**
+   * Registered user's email id
+   */
   email: string;
+  /**
+   * Registered user's mobile number
+   */
   mobile: string;
 }
 
